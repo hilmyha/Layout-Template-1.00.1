75036ba380dbbb413bf970f2e7fc15d4dd101c67
Haidar <68181532+hilmyha@users.noreply.github.com>
2021-09-01 21:02:04 +0700

    add scroll button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,9 @@ const nav = document.querySelector('.nav-menu');
 navToggle.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
 });
+
+const btnScroll = document.querySelector('.scroll-top');
+
+btnScroll.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+})
